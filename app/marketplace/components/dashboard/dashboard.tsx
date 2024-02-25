d4a0d9aacc01062b840648e05d2c311acf8b36ee
Truong Nguyen <truongnguyenptn@gmail.com>
2024-02-25 15:27:28 +0700

    feat: update nft market

--- app/marketplace/components/dashboard/dashboard.tsx
+++ app/marketplace/components/dashboard/dashboard.tsx
@@ -1,88 +1,90 @@
-import "./dashboard.css"
-import Trending from "../trending/trending"
-import Featured from "./images/Featured.svg"
-import Avatar from "./images/Avatar.svg"
+import "./dashboard.css";
+import Trending from "../trending/trending";
+import Featured from "./images/Featured.svg";
+import Avatar from "./images/Avatar.svg";
 
-import Image from "next/image"
+import Image from "next/image";
 
-type nftData = any[] | null
+type nftData = any[] | null;
 
-const Dashboard = ({displayMode, data}: {displayMode: string, data: nftData}) => {
+const Dashboard = ({
+  displayMode,
+  data,
+}: {
+  displayMode: string;
+  data: nftData;
+}) => {
+  let featuredClass;
+  let contContainerClass;
+  let discTitleClass;
 
-    let featuredClass
-    let contContainerClass
-    let discTitleClass
-    
-    if (displayMode === 'dark') {
-        featuredClass = "featured"
-        contContainerClass = "cont--container"
-        discTitleClass = "disc--title"
-    } else {
-        featuredClass = "featured-lm"
-        contContainerClass = "cont--container-lm"
-        discTitleClass = "disc--title-lm"
-    }
+  if (displayMode === "dark") {
+    featuredClass = "featured";
+    contContainerClass = "cont--container";
+    discTitleClass = "disc--title";
+  } else {
+    featuredClass = "featured-lm";
+    contContainerClass = "cont--container-lm";
+    discTitleClass = "disc--title-lm";
+  }
 
-    // console.log(data)
+  // console.log(data)
 
-    return(
-        <main className={contContainerClass}>
-            <div className="discover--container">
-                <div id="explore" className="discover">
-                    <div className={discTitleClass}>
-                        Discover, Collect, Sell, and HODL NFT
-                    </div>
-                    <div id="desc">
-                        Digital marketplace for crypto NFT HODLer 
-                    </div>
-                    <div className="button--container">
-                        <button id="discover">Explore</button>
-                        <button id="create">Create</button>
-                    </div>
-                </div>
-                <div id="explore" className={featuredClass}>
-                    <Image id="featured" src={Featured} alt="featured"/>
-                    <div className="ft--container">
-                        <div className="ft--user-container">
-                            <Image src={Avatar} alt="avatar"/>
-                            <div>John Abraham</div>
-                            <div id="status"></div>
-                        </div>
-                        <div className="ft--title">Brighten LQ</div>
-                        <div className="ft--auction">
-                            <div>Auction time</div>
-                            <div id="current-bid">
-                                <div>Current HODL</div>
-                                <div>:</div>
-                                <div id="bid">1.50 SOL</div>
-                            </div>
-                        </div>
-                        <div className="vals">
-                            <div>3h 1m 50s</div>
-                            <div>20 SOL</div>
-                        </div>
-                        <div className="button--container">
-                            <button id="place-bid">Place a Bid</button>
-                            <button id="details">Details</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <main className={contContainerClass}>
+      <div className="discover--container">
+        <div id="explore" className="discover">
+          <div className={discTitleClass}>
+            Discover, Collect, Sell, and HODL NFT
+          </div>
+          <div id="desc">Digital marketplace for crypto NFT HODLer</div>
+          <div className="button--container">
+            <button id="discover">Explore</button>
+            <button id="create">Create</button>
+          </div>
+        </div>
+        <div id="explore" className={featuredClass}>
+          <Image id="featured" src={Featured} alt="featured" />
+          <div className="ft--container">
+            <div className="ft--user-container">
+              <Image src={Avatar} alt="avatar" />
+              <div>John Abraham</div>
+              <div id="status"></div>
             </div>
-            <div className="label">
-                <div>Trending HODL</div>
-                <div id="selector">
-                    <div id="selected" className="selector--item">All</div>
-                    <div className="selector--item">Artwork</div>
-                    <div className="selector--item">Music</div>
-                </div>
+            <div className="ft--title">Brighten LQ</div>
+            <div className="ft--auction">
+              <div>Auction time</div>
+              <div id="current-bid">
+                <div>Current HODL</div>
+                <div>:</div>
+                <div id="bid">1.50 SOL</div>
+              </div>
             </div>
-        
-            <Trending
-                displayMode={displayMode}
-                data={data}
-            />
-        </main>
-    )
-}
+            <div className="vals">
+              <div>3h 1m 50s</div>
+              <div>20 SOL</div>
+            </div>
+            <div className="button--container">
+              <button id="place-bid">Place a proposal</button>
+              <button id="details">Details</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="label">
+        <div>Trending HODL</div>
+        <div id="selector">
+          <div id="selected" className="selector--item">
+            All
+          </div>
+          <div className="selector--item">Artwork</div>
+          <div className="selector--item">Music</div>
+        </div>
+      </div>
+
+      <Trending displayMode={displayMode} data={data} />
+    </main>
+  );
+};
 
 export default Dashboard;
